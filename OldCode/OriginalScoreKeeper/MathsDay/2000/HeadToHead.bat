24e4ab4278e56c800bbb207975a37c621085bb4f
vmangalath <vishnu.mangalath@gmail.com>
2025-05-29 17:53:16 +1000

    organise

---
+++ OldCode/OriginalScoreKeeper/MathsDay/2000/HeadToHead.bat
@@ -0,0 +1,12 @@
+@Echo off
+Echo ***** Please wait till invited to close DOS Window ....
+Echo -----------------------------------------------------------------------
+C:
+cd C:\ScoreKeeper\MATHSDAY\2000
+Echo Copying Head to Head Input from DB Directory to System Directory
+Copy C:\ScoreKeeper\MATHSDAY\2000\SwissHTHin5.txt "C:\ScoreKeeper\MATHSDAY\2000\data.txt"
+Echo Running Java to Determine Head to Head Round 5
+"C:\Program Files\JavaSoft\JRE\1.3.0_02\bin\java" Swiss > C:\ScoreKeeper\MATHSDAY\2000\SwissHTHOut5.txt
+Echo Completed execution of Java Script to Determine Teams to meet Head to Head
+Echo -----------------------------------------------------------------------
+Echo ***** Close this DOS Window and Continue running Scorekeeper ......
